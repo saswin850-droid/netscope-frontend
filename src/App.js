fb50a32dcb3dbd822d56ed9ae67b8cfee21ef3ed
Aswin <saswin@gmail.com>
2026-05-10 10:34:58 +0530

    fix eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {
   useState, useEffect, useRef, useCallback, useMemo,
 } from "react";
@@ -326,6 +327,7 @@ export default function App() {
     });
 
     return () => socket.disconnect();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const startScan = useCallback(() => {
